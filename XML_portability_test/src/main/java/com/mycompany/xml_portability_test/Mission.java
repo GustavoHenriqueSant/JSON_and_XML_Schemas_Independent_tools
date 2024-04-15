ec1b7a7cf7b8bca6bc7b47f0c1c6124727c500cb
freshSant <gustavo.git@hotmail.com>
2024-04-15 11:56:54 -0300

    XML portability test

---
+++ XML_portability_test/src/main/java/com/mycompany/xml_portability_test/Mission.java
@@ -0,0 +1,73 @@
+package com.mycompany.xml_portability_test;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "mission")
+@XmlType(propOrder = {"id", "purpose", "method", "goals", "project_id", "created_at", "updated_at"})
+public class Mission {
+    private int id;
+    private String purpose;
+    private String method;
+    private String goals;
+    private int project_id;
+    private String created_at;
+    private String updated_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getGoals() {
+        return goals;
+    }
+
+    public void setGoals(String goals) {
+        this.goals = goals;
+    }
+
+    public int getProject_id() {
+        return project_id;
+    }
+
+    public void setProject_id(int project_id) {
+        this.project_id = project_id;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+}
---
+++ XML_portability_test/src/main/java/com/mycompany/xml_portability_test/Rule.java
@@ -0,0 +1,65 @@
+package com.mycompany.xml_portability_test;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "rule")
+@XmlType(propOrder = {"id", "controlaction_id", "column", "created_at", "updated_at", "variable_state_relations"})
+public class Rule {
+    private int id;
+    private int controlaction_id;
+    private String column;
+    private String created_at;
+    private String updated_at;
+    private Variable_state_relation variable_state_relations;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getControlaction_id() {
+        return controlaction_id;
+    }
+
+    public void setControlaction_id(int controlaction_id) {
+        this.controlaction_id = controlaction_id;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public Variable_state_relation getVariable_state_relations() {
+        return variable_state_relations;
+    }
+
+    @XmlElement(name = "variable_state_relations")
+    public void setVariable_state_relations(Variable_state_relation variable_state_relations) {
+        this.variable_state_relations = variable_state_relations;
+    }
+}
