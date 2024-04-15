0726b98dc91bc5dcda94fea3297af5766df1b34e
freshSant <gustavo.git@hotmail.com>
2024-04-15 00:18:22 -0300

    JSON portability test

---
+++ JSON_portability_test/src/main/java/com/mycompany/json_portability_test/Main.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.json_portability_test;
+
+import com.google.gson.Gson;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try{
+            //Reading the STPA analysis from the JSON file and transforming it into a Project object, including all relationships between entities.
+            String json =  new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir") + "\\assets\\TrainDoosdrSystem.json")));
+            Gson gson_object = new Gson();
+            Project projeto = gson_object.fromJson(json, Project.class);
+            //==================================================================================================================================================================================
+            //Modification of the original analysis: addition of a new system goal, and change in the project name
+            projeto.setName("ATT Train Door System JSON");
+            System_goals sg = new System_goals(999, "Teste", "Description", 1, "2021-09-21 17:27:12", "2021-09-21 17:27:12");
+            projeto.getSystem_goals().add(sg);
+            //==================================================================================================================================================================================
+            //Saving modified analysis in this tool, to be imported later by WebSTAMP
+            json = gson_object.toJson(projeto);
+            FileWriter fileWriter = new FileWriter(System.getProperty("user.dir") + "\\assets\\AttJSON_TrainDoorSystem.json");
+            fileWriter.write(json);
+            fileWriter.close();
+            System.out.print("Finalizado");
+        } catch (FileNotFoundException | NoSuchFileException e){
+            System.out.println("File not found.");
+        } catch (Exception e) {
+            System.out.println("An error occurred while processing the file.");
+        }
+    }
+}
---
+++ JSON_portability_test/src/main/java/com/mycompany/json_portability_test/System_safety_constraint_hazards_relations.java
@@ -0,0 +1,51 @@
+package com.mycompany.json_portability_test;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class System_safety_constraint_hazards_relations {
+    private int ssc_id;
+    private int hazard_id;
+    private String created_at;
+    private String updated_at;
+
+    public System_safety_constraint_hazards_relations(int ssc_id, int hazard_id, String created_at, String updated_at) {
+        this.ssc_id = ssc_id;
+        this.hazard_id = hazard_id;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    } 
+    
+    public int getSsc_id() {
+        return ssc_id;
+    }
+
+    public void setSsc_id(int ssc_id) {
+        this.ssc_id = ssc_id;
+    }
+
+    public int getHazard_id() {
+        return hazard_id;
+    }
+
+    public void setHazard_id(int hazard_id) {
+        this.hazard_id = hazard_id;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }   
+}
